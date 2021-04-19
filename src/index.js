a3aee377caf79172143f573df3d94c30e6fcd162
Omar Nachar <omarnachar2@gmail.com>
2021-04-19 07:39:45 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    let inputTag = document.querySelector('input#new-task-description');
+    let inputValue = inputTag.value;
+
+
+    let newLi = document.createElement('li');
+    newLi.innerText = inputValue;
+
+    let ulTag = document.querySelector('ul#tasks');
+    ulTag.append(newLi);
+    inputTag.value = '';
+
+  })
 });
